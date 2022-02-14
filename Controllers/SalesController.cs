9906b583b5df9a26c7040e493076cbba25d32904
Zeph <91007363+sttan2020@users.noreply.github.com>
2022-02-14 14:48:09 +0800

    Add files via upload

---
+++ Controllers/SalesController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Project1.Models;
+
+namespace Project1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SalesController : ControllerBase
+    {
+        private readonly TalentContext _context;
+
+        public SalesController(TalentContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Sales
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Sales>>> GetSales()
+        {
+            //return await _context.Sales.ToListAsync();
+
+
+            var data = await _context.Sales
+               .Include(s => s.Customer)
+               .Include(s => s.Product)
+               .Include(s => s.Store)
+                .Select(s => new { id = s.Id, customerName = s.Customer.Name, productName = s.Product.Name, storeName = s.Store.Name, dateSold = s.DateSold })
+               .ToListAsync();
+
+            return Ok(data);
+        }
+
+        // GET: api/Sales/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Sales>> GetSales(int id)
+        {
+            var sales = await _context.Sales.FindAsync(id);
+
+            if (sales == null)
+            {
+                return NotFound();
+            }
+
+            return sales;
+        }
+
+        // PUT: api/Sales/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSales(int id, Sales sales)
+        {
+            if (id != sales.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(sales).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SalesExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Sales
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Sales>> PostSales(Sales sales)
+        {
+
+            _context.Sales.Add(sales);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSales", new { id = sales.Id }, sales);
+        }
+
+        // DELETE: api/Sales/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Sales>> DeleteSales(int id)
+        {
+            var sales = await _context.Sales.FindAsync(id);
+            if (sales == null)
+            {
+                return NotFound();
+            }
+
+            _context.Sales.Remove(sales);
+            await _context.SaveChangesAsync();
+
+            return sales;
+        }
+
+        private bool SalesExists(int id)
+        {
+            return _context.Sales.Any(e => e.Id == id);
+        }
+    }
+}
